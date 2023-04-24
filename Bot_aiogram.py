89c20db2b02c794e1dbcefee44584f584d665389
feodorrussia <fvdelows@mail.ru>
2023-04-25 02:21:26 +0300

    Restructure with new create_bot file and fix some problems with importing of handlers

--- Bot_aiogram.py
+++ Bot_aiogram.py
@@ -1,28 +1,10 @@
-import logging
-from aiogram import Bot
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
-from other.config_reader import config
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from create_bot import *
 
-from handlers import client, admin
+from handlers.client import *
+from handlers.admin import *
 
-from sqlalchemy.orm import sessionmaker
-from data.DataBase_setup import *
-
-engine = create_engine('sqlite:///data/database.db')
-Base.metadata.bind = engine
-
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
-
-storage = MemoryStorage()
-logging.basicConfig(level=logging.INFO)
-bot = Bot(token=config.bot_token.get_secret_value())  # безопашношть
-dp = Dispatcher(bot, storage=storage)
-
-client.register_handlers_client(dp)
-admin.register_handlers_admin(dp)
+register_handlers_client(dp)
+register_handlers_admin(dp)
 
 
 if __name__ == '__main__':
