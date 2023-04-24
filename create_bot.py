d54b968f092ee01f6735ec757e1fe1f23a8bb475
feodorrussia <fvdelows@mail.ru>
2023-04-25 02:19:52 +0300

    Create new file to running bot (it will be run from Bot_aiogram in the start)

---
+++ create_bot.py
@@ -0,0 +1,19 @@
+import logging
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from sqlalchemy.orm import sessionmaker
+from data.DataBase_setup import *
+
+engine = create_engine('sqlite:///data/database.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+storage = MemoryStorage()
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token="6218189424:AAF4Upm5NsSzYh37T-UCUggbuGwSO5evPG8")  #
+dp = Dispatcher(bot, storage=storage)
