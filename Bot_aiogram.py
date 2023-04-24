bdbe57281bf525d9a70666769f11dda56245cdbd
feodorrussia <fvdelows@mail.ru>
2023-04-25 02:36:31 +0300

    Beauty

--- Bot_aiogram.py
+++ Bot_aiogram.py
@@ -1,11 +1,10 @@
 from create_bot import *
-
 from handlers.client import *
 from handlers.admin import *
 
+
 register_handlers_client(dp)
 register_handlers_admin(dp)
-
 
 if __name__ == '__main__':
     executor.start_polling(dp)
