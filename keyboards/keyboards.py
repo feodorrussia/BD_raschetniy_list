9bdad4837d36bdfbeb43f181e4d8f6a77b9d89a4
feodorrussia <fvdelows@mail.ru>
2023-04-25 16:19:46 +0300

    Redo kb_admin_def with row_width=2

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -4,11 +4,10 @@ kb_hello = types.ReplyKeyboardMarkup(resize_keyboard=True)
 but1 = types.KeyboardButton("Привет!")
 kb_hello.add(but1)
 
-kb_admin_def = types.InlineKeyboardMarkup(resize_keyboard=True)
+kb_admin_def = types.InlineKeyboardMarkup(row_width=2, resize_keyboard=True)
 buttons = [types.InlineKeyboardButton(text="Добавление/назначение", callback_data="add_default"),
            types.InlineKeyboardButton(text="Изменение", callback_data="upd_default"),
            types.InlineKeyboardButton(text="Увольнение/удаление", callback_data="del_default"),
            types.InlineKeyboardButton(text="Генерация", callback_data="gen_default")]
-for butt in buttons:
-    kb_admin_def.add(butt)
+kb_admin_def.add(*buttons)
 
