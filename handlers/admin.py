2365219239f95d86669721c2be927315c92433f1
feodorrussia <fvdelows@mail.ru>
2023-04-27 20:14:09 +0300

    Edit default messages of callbacks for default buttons (& redo callback register)

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,6 +1,6 @@
-from aiogram import Dispatcher
-from create_bot import bot
-from keyboards.keyboards import *
+from aiogram import types, Dispatcher
+from create_bot import bot, dp
+from keyboards.keyboards import kb_cancel, kb_admin_def, kb_add_def, kb_del_def, kb_gen_def, kb_upd_def
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from states.AdminStatus import AdminStatus
@@ -10,29 +10,29 @@ from states.AdminStatus import AdminStatus
 async def menu_handler(message: types.Message, state : FSMContext):
     chat_id = message.chat.id
     # await remove_chat_buttons(chat_id)
-    await bot.send_message(chat_id, "What do you want?", reply_markup=kb_admin_def)
+    await bot.send_message(chat_id, "Чего изволите?\nВыйти - /exit", reply_markup=kb_admin_def)
 
-# @bot.callback_query_handler(text="add_default")
-async def add_default(call: types.CallbackQuery):
-    await call.message.answer("Go & fuck yourself, bitch")
+# @dp.callback_query_handler(Text("add_default"))
+async def add_default_handler(call: types.CallbackQuery):
+    await call.message.answer("Выберите, что/кого Вы хотите добавить\nМеню - /start_menu", reply_markup=kb_add_def)
     await call.answer()
 
 
 # @bot.callback_query_handler(text="upd_default")
-async def upd_default(call: types.CallbackQuery):
-    await call.message.answer("Go & fuck yourself, bitch")
+async def upd_default_handler(call: types.CallbackQuery):
+    await call.message.answer("Выберите, что/кого Вы хотите изменить\nМеню - /start_menu", reply_markup=kb_upd_def)
     await call.answer()
 
 
 # @bot.callback_query_handler(text="del_default")
-async def del_default(call: types.CallbackQuery):
-    await call.message.answer("Go & fuck yourself, bitch")
+async def del_default_handler(call: types.CallbackQuery):
+    await call.message.answer("Выберите, что/кого Вы хотите удалить/уволить\nМеню - /start_menu", reply_markup=kb_del_def)
     await call.answer()
 
 
 # @bot.callback_query_handler(text="gen_default")
-async def gen_default(call: types.CallbackQuery):
-    await call.message.answer("Go & fuck yourself")
+async def gen_default_handler(call: types.CallbackQuery):
+    await call.message.answer("Выберите, что Вы хотите сгенерировать\nМеню - /start_menu", reply_markup=kb_gen_def)
     await call.answer()
 
 
@@ -42,10 +42,10 @@ async def cancel_handler(message : types.Message, state : FSMContext):
         return
     if current_state == "AdminStatus:authorizing":
         await AdminStatus.unauthorized.set()
-        await message.reply("No problem, bro")
+        await message.reply("Как скажете, Барин.\nАвторизоваться снова - /admin", reply_markup=types.ReplyKeyboardRemove())
         return
     await AdminStatus.authorized.set()
-    await message.reply("No problem, bro")
+    await message.reply("Как скажете, Барин.\nМеню - /start_menu", reply_markup=types.ReplyKeyboardRemove())
 
 
 async def exit_handler(message : types.Message, state : FSMContext):
@@ -53,17 +53,17 @@ async def exit_handler(message : types.Message, state : FSMContext):
     if current_state is None:
         return
     await AdminStatus.unauthorized.set()
-    await message.reply("No problem, bro. U r free now")
+    await message.reply("Как скажете, Барин.\nАвторизоваться - /admin", reply_markup=types.ReplyKeyboardRemove())
 
 def register_handlers_admin(dp : Dispatcher):
     dp.register_message_handler(menu_handler, commands=['start_menu'], state=AdminStatus.authorized)
     dp.register_message_handler(menu_handler, Text(equals="меню", ignore_case=True), state=AdminStatus.authorized)
-    dp.register_message_handler(cancel_handler, state="*", commands='cancel')
+    dp.register_message_handler(cancel_handler, state="*", commands=['cancel'])
     dp.register_message_handler(cancel_handler, Text(equals="отмена", ignore_case=True), state="*")
-    dp.register_message_handler(exit_handler, state="*", commands='exit')
-    dp.register_message_handler(exit_handler, Text(equals="выйти", ignore_case=True), state="*")
+    dp.register_message_handler(exit_handler, state=AdminStatus.authorized, commands=['exit'])
+    dp.register_message_handler(exit_handler, Text(equals="выйти", ignore_case=True), state=AdminStatus.authorized)
 
-    dp.register_callback_query_handler(add_default)
-    dp.register_callback_query_handler(upd_default)
-    dp.register_callback_query_handler(del_default)
-    dp.register_callback_query_handler(gen_default)
+    dp.register_callback_query_handler(add_default_handler, lambda call: call.data == "add_default", state=AdminStatus.authorized)
+    dp.register_callback_query_handler(upd_default_handler, lambda call: call.data == "upd_default", state=AdminStatus.authorized)
+    dp.register_callback_query_handler(del_default_handler, lambda call: call.data == "del_default", state=AdminStatus.authorized)
+    dp.register_callback_query_handler(gen_default_handler, lambda call: call.data == "gen_default", state=AdminStatus.authorized)
