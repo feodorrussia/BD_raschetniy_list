338ab0653aa9e474ba7519d1cb6ddb9fa3c255ea
feodorrussia <fvdelows@mail.ru>
2023-04-27 20:15:07 +0300

    Connect Add_processing

--- Bot_aiogram.py
+++ Bot_aiogram.py
@@ -1,10 +1,15 @@
 from create_bot import *
 from handlers.client import *
 from handlers.admin import *
+from handlers.Add_proccessing import *
+from handlers.Delete_proccessing import *
+from handlers.Generation_proccessing import *
+from handlers.Update_proccessing import *
 
 
 register_handlers_client(dp)
 register_handlers_admin(dp)
+register_handlers_add(dp)
 
 if __name__ == '__main__':
     executor.start_polling(dp)
