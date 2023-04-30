5a90630a8324fd892d3f24b8696dce5b1194a539
feodorrussia <fvdelows@mail.ru>
2023-04-30 21:00:24 +0300

    Add message handlers for main menus

--- handlers/admin.py
+++ handlers/admin.py
@@ -17,11 +17,17 @@ async def add_default_handler(call: types.CallbackQuery):
     await call.message.answer("Выберите, что/кого Вы хотите добавить\nМеню - /start_menu", reply_markup=kb_add_def)
     await call.answer()
 
+async def add_menu_handler(message: types.Message):
+    await bot.send_message("Выберите, что/кого Вы хотите добавить\nМеню - /start_menu", reply_markup=kb_add_def)
+
 
 # @bot.callback_query_handler(text="upd_default")
 async def upd_default_handler(call: types.CallbackQuery):
     await call.message.answer("Выберите, что/кого Вы хотите изменить\nМеню - /start_menu", reply_markup=kb_upd_def)
     await call.answer()
+
+async def upd_menu_handler(message: types.Message):
+    await bot.send_message("Выберите, что/кого Вы хотите изменить\nМеню - /start_menu", reply_markup=kb_upd_def)
 
 
 # @bot.callback_query_handler(text="del_default")
@@ -29,11 +35,17 @@ async def del_default_handler(call: types.CallbackQuery):
     await call.message.answer("Выберите, что/кого Вы хотите удалить/уволить/завершить\nМеню - /start_menu", reply_markup=kb_del_def)
     await call.answer()
 
+async def del_menu_handler(message: types.Message):
+    await bot.send_message("Выберите, что/кого Вы хотите удалить/уволить/завершить\nМеню - /start_menu", reply_markup=kb_del_def)
+
 
 # @bot.callback_query_handler(text="gen_default")
 async def gen_default_handler(call: types.CallbackQuery):
     await call.message.answer("Выберите, что Вы хотите сгенерировать\nМеню - /start_menu", reply_markup=kb_gen_def)
     await call.answer()
+
+async def gen_menu_handler(message: types.Message):
+    await bot.send_message("Выберите, что Вы хотите сгенерировать\nМеню - /start_menu", reply_markup=kb_gen_def)
 
 
 async def cancel_handler(message : types.Message, state : FSMContext):
@@ -63,7 +75,15 @@ def register_handlers_admin(dp : Dispatcher):
     dp.register_message_handler(exit_handler, state=AdminStatus.authorized, commands=['exit'])
     dp.register_message_handler(exit_handler, Text(equals="выйти", ignore_case=True), state=AdminStatus.authorized)
 
+
+    dp.register_message_handler(add_menu_handler, commands=['add_menu'], state=AdminStatus.authorized)
     dp.register_callback_query_handler(add_default_handler, lambda call: call.data == "add_default", state=AdminStatus.authorized)
+
+    dp.register_message_handler(upd_menu_handler, commands=['upd_menu'], state=AdminStatus.authorized)
     dp.register_callback_query_handler(upd_default_handler, lambda call: call.data == "upd_default", state=AdminStatus.authorized)
+
+    dp.register_message_handler(del_menu_handler, commands=['del_menu'], state=AdminStatus.authorized)
     dp.register_callback_query_handler(del_default_handler, lambda call: call.data == "del_default", state=AdminStatus.authorized)
+
+    dp.register_message_handler(gen_menu_handler, commands=['gen_menu'], state=AdminStatus.authorized)
     dp.register_callback_query_handler(gen_default_handler, lambda call: call.data == "gen_default", state=AdminStatus.authorized)
