b9a3a9ec8589c9f01274a146bcee751ef4450ee8
feodorrussia <fvdelows@mail.ru>
2023-04-30 20:13:30 +0300

    Create all deleting states

--- states/DeletingStates.py
+++ states/DeletingStates.py
@@ -3,3 +3,23 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 class FireEmployee(StatesGroup):
     name_employee = State()
     date_fire = State()
+
+class EndContract(StatesGroup):
+    name = State()
+    check = State()
+    deleting = State()
+
+class DeleteChild(StatesGroup):
+    name_employee = State()
+    check = State()
+    deleting = State()
+
+class DeleteAward(StatesGroup):
+    name = State()
+    check = State()
+    deleting = State()
+
+class DeletePosition(StatesGroup):
+    name = State()
+    check = State()
+    deleting = State()
