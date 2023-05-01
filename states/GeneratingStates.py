bc5c2d6229228c2df1c699babc08daf801b294f4
feodorrussia <fvdelows@mail.ru>
2023-05-02 00:57:36 +0300

    Init Generating states

---
+++ states/GeneratingStates.py
@@ -0,0 +1,10 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+# class GenerateWarning(StatesGroup):
+#     name_employee = State()
+
+class GenerateEmployeeProfit(StatesGroup):
+    name_employee = State()
+
+# class GenerateProfit(StatesGroup):
+#     name_employee = State()
