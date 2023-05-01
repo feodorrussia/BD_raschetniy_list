9fcd371375bf769d9723ee60c2f7214b02a5acb2
feodorrussia <fvdelows@mail.ru>
2023-05-02 01:25:44 +0300

    Little rename in generating buttons

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -32,7 +32,7 @@ kb_del_def.add(*buttons_3)
 kb_gen_def = types.InlineKeyboardMarkup(row_width=2, resize_keyboard=True)
 buttons_4 = [types.InlineKeyboardButton(text="Список вакансий", callback_data="gen_vacancy"),
             types.InlineKeyboardButton(text="Расчётный лист сотрудника", callback_data="gen_employee_profit"),
-            types.InlineKeyboardButton(text="Письмо предупреждения", callback_data="gen_warning"),
+            types.InlineKeyboardButton(text="Предупреждения", callback_data="gen_warning"),
             types.InlineKeyboardButton(text="Годовой доход всех сотрудников", callback_data="gen_profit")]
 kb_gen_def.add(*buttons_4)
 
