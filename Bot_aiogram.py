bbd99e99f781e4d83da911aaee3ae3b092ca9a9c
feodorrussia <fvdelows@mail.ru>
2023-05-02 01:00:09 +0300

    Connect editing & generating handlers

--- Bot_aiogram.py
+++ Bot_aiogram.py
@@ -1,16 +1,18 @@
-from create_bot import *
-from handlers.client import *
-from handlers.admin import *
-from handlers.Add_proccessing import *
-from handlers.Delete_proccessing import *
-from handlers.Generation_proccessing import *
-from handlers.Update_proccessing import *
+from create_bot import dp, executor
+from handlers.client import register_handlers_client
+from handlers.admin import register_handlers_admin
+from handlers.Add_processing import register_handlers_add
+from handlers.Delete_processing import register_handlers_delete
+from handlers.Generate_processing import register_handlers_generate
+from handlers.Edit_processing import register_handlers_edit
 
 
 register_handlers_client(dp)
 register_handlers_admin(dp)
 register_handlers_add(dp)
 register_handlers_delete(dp)
+register_handlers_generate(dp)
+register_handlers_edit(dp)
 
 if __name__ == '__main__':
     executor.start_polling(dp)
