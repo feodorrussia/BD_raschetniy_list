7e62fbcc259a638e9216297f9310dde248b6a6ea
feodorrussia <fvdelows@mail.ru>
2023-05-11 00:21:33 +0300

    Set higher priority to generate handlers

--- Bot_aiogram.py
+++ Bot_aiogram.py
@@ -9,9 +9,9 @@ from handlers.Edit_processing import register_handlers_edit
 
 register_handlers_client(dp)
 register_handlers_admin(dp)
+register_handlers_generate(dp)
 register_handlers_add(dp)
 register_handlers_delete(dp)
-register_handlers_generate(dp)
 register_handlers_edit(dp)
 
 if __name__ == '__main__':
