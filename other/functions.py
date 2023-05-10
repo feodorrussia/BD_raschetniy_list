012ea34d66f24a00b7b98427b23916446a4d0e61
feodorrussia <fvdelows@mail.ru>
2023-05-11 01:38:33 +0300

    Fix some bugs in check date & create function to find date difference

--- other/functions.py
+++ other/functions.py
@@ -2,18 +2,33 @@ import datetime
 
 
 def check_date(date):
-    date = date.strip().split()
+    try:
+        date = list(map(int, date.strip().split(".")))
+    except Exception as e:
+        return False
+    date_now = datetime.date.today()
     if len(date) == 3:
-        if 0 < date[0] <= 31 and 0 < date[1] <= 12 and 0 < date[2] <= datetime.date.today().year:
-            if date[2] < datetime.date.today().year:
+        if 0 < date[0] <= 31 and 0 < date[1] <= 12 and 0 < date[2] <= date_now.year:
+            if date[2] < date_now.year:
                 return True
-            elif date[2] == datetime.date.today().year:
-                if date[1] < datetime.date.today().month:
+            elif date[2] == date_now.year:
+                if date[1] < date_now.month:
                     return True
-                elif date[1] == datetime.date.today().month:
-                    if date[0] <= datetime.date.today().day:
+                elif date[1] == date_now.month:
+                    if date[0] <= date_now.day:
                         return True
     return False
+
+def dif_date(date1, date2):
+    exp = [date2.year - date1.year, date2.month - date1.month, date2.day - date1.day]
+
+    exp[1] += - 1 if exp[2] < 0 else 0
+    exp[0] += -1 if exp[1] < 0 else 0
+    exp[1] = (12 + exp[1]) % 12
+    exp[2] = (date2 - datetime.date(date2.year - (0 if 12 > date2.month - 1 > 0 else 1), (12 + date2.month - 2) % 12 + 1,
+                                   date1.day)).days
+    # print(exp)
+    return exp
 
 
 def generate_employee_list(employees):
